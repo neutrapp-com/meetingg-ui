cdfdfe73f7d5fbdbe5cef7005f388c9b66c76355
Florian Cain <flocain51@gmail.com>
2021-03-14 23:40:14 +0100

    🎥 Début caméra call

---
+++ src/components/widgets/call/Camera.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="camera">
+        <div class="informations">
+            <div>
+                <p class="ml-4 bg">{{participant.lastname + ' ' + participant.firstname}}</p>
+            </div>
+            <div class="flex">
+                <ion-icon class="text-gray-400 text-xl mr-2 bg" :name="participant.audio ? 'mic-outline': 'mic-off-outline'"></ion-icon>
+                <ion-icon class="text-gray-400 text-xl bg" :name="participant.video ? 'eye-outline': 'eye-off-outline'"></ion-icon>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            participant:{
+                type:Object,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.camera{
+    @apply flex;
+    align-items: end;
+    @apply bg-cover;
+    min-width: 33.33%;
+    background-image: url('https://ak.picdn.net/shutterstock/videos/26198027/thumb/12.jpg');
+    
+
+    .informations{
+        @apply flex w-full;
+        padding: 0% 5% 2% 0%;
+        justify-content: space-between;
+
+        .bg{
+            background-color: rgba(0, 0, 0, 0.4);
+            padding: 2px;
+            border-radius: 5px;
+        }
+    }
+
+    
+}
+
+
+
+
+</style>
--- src/components/widgets/call/ParticipantContainer.vue
+++ src/components/widgets/call/ParticipantContainer.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="flex flex-col bg-red-600 w-full">
-        <div v-for="participant in participants" v-bind:key="participant.id" class="bg-red-200 w-4/5 h-full flex grow">
-            eee
+        <!--<div v-for="participant in participants" v-bind:key="participant.id" class="bg-red-200 w-4/5 h-full flex grow">-->
+        <div v-for="participant in participants" v-bind:key="participant.id" class="h-full flex">
+            <camera :participant="participant"/>
         </div>
     </div>
 </template>
 
 <script>
+    import Camera from './Camera.vue';
     export default {
+        components:{
+            Camera
+        },
         props:{
             participants:{
                 type:Array
